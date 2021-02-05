5c735fbf5681108e3afaee6a186f5fd5c7314ec7
Jared Hanson <jaredhanson@gmail.com>
2021-02-05 12:10:09 -0800

    Simplify iron token usage.

---
+++ app/iron.js
@@ -0,0 +1,6 @@
+exports = module.exports = function() {
+  return require('tokens-iron');
+};
+
+exports['@implements'] = 'http://i.bixbyjs.org/security/tokens/Format';
+exports['@type'] = 'application/fe26.2';
--- test/package.test.js
+++ test/package.test.js
@@ -6,11 +6,11 @@ describe('bixby-security-tokens-confidential', function() {
     var json = require('../package.json');
     
     it('should have assembly metadata', function() {
-      expect(json.assembly.namespace).to.equal('tokens');
+      expect(json.assembly.namespace).to.equal('security/tokens');
       
       expect(json.assembly.components).to.have.length(2);
       expect(json.assembly.components).to.include('fernet/token');
-      expect(json.assembly.components).to.include('iron/token');
+      expect(json.assembly.components).to.include('iron');
     });
   });
   
